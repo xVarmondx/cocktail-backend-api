4d7406d8871a55a403bddf916a989263185e0248
Paweł Z <pawelzieba251@gmail.com>
2026-03-23 00:57:10 +0100

    fix: authorization and add requirements.txt

--- cocktails/views.py
+++ cocktails/views.py
@@ -2,6 +2,13 @@ from rest_framework import viewsets, permissions
 from .models import Ingredient, Cocktail
 from .serializers import IngredientSerializer, CocktailSerializer
 
+class IsOwnerOrAdmin(permissions.BasePermission):
+    """Pozwala na edycję/usunięcie tylko autorowi lub adminowi."""
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return obj.author == request.user or request.user.is_staff
+
 
 class IngredientViewSet(viewsets.ModelViewSet):
     queryset = Ingredient.objects.all()
@@ -16,7 +23,7 @@ class IngredientViewSet(viewsets.ModelViewSet):
 class CocktailViewSet(viewsets.ModelViewSet):
     queryset = Cocktail.objects.all()
     serializer_class = CocktailSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrAdmin]
 
     filterset_fields = ['category', 'author']
     search_fields = ['name', 'instructions']
